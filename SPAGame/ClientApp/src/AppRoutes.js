da004b4180463e17b46bd5e156b43b8175435d80
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2024-02-19 13:23:24 +0100

    updated .gitignore and made some other smaller changes

--- SPAGame/ClientApp/src/AppRoutes.js
+++ SPAGame/ClientApp/src/AppRoutes.js
@@ -5,16 +5,16 @@ import { Home } from "./components/Home";
 const AppRoutes = [
   {
     index: true,
-    element: <Home />
+    element: <Home />,
   },
   {
-    path: '/counter',
-    element: <Counter />
+    path: "/counter",
+    element: <Counter />,
   },
   {
-    path: '/fetch-data',
-    element: <FetchData />
-  }
+    path: "/fetch-data",
+    element: <FetchData />,
+  },
 ];
 
 export default AppRoutes;
