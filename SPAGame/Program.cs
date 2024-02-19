b0a23be8349c562ea9ecd25a986c0ef62c4df313
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2024-02-19 10:46:28 +0100

    updated identity API

--- SPAGame/Program.cs
+++ SPAGame/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SPAGame.Data;
 
@@ -13,6 +14,21 @@ namespace SPAGame
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                             options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContext")));
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddIdentityCore<IdentityUser>()
+                .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("SPAGame")
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+            });
 
             var app = builder.Build();
 
